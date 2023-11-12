2b2f67f894c4ff1c405bd3cbce9917074e372b46
Jonas Sena <sena.jonas@outlook.com>
2023-11-12 18:09:30 -0300

    Seleção de planetas

--- Assets/Devs/Jonas/Scripts/PlanetSelection.cs
+++ Assets/Devs/Jonas/Scripts/PlanetSelection.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlanetSelection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //[SerializeField]
+    //private List<SceneAsset> Planetas;
+    string planetName;
+
+    private void Start()
     {
-        
+        planetName = GetComponentInChildren<TextMeshProUGUI>().text;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void selectPlanet()
     {
-        
+        SceneManager.LoadScene(planetName);
     }
 }
